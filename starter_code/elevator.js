c1b59493172bd765e6f6feb21733ac2d7b7393e0
Gon <jamesfog@Gons-MacBook-Pro.local>
2017-12-19 16:58:59 +0100

    first commit

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,27 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() { setInterval(this.update, 1000)}
+  stop() { clearInterval(start)}
+  update() { this.log(
+
+  )}
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
+  floorUp() {
+    if(this.floor < 10){
+      this.floor += 1;
+    }
+    if(this.floor > 0){
+      this.floor -= 1;
+    }
+  }
   floorDown() { }
   call() { }
-  log() { }
+  log() { console.log(this.floor, this.direction) }
 }
 
 module.exports = Elevator;
